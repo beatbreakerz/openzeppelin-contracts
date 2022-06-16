f309bf5d9e0890beced7b7ba89cfe7e6d187dd33
beatbreakerz <54270916+beatbreakerz@users.noreply.github.com>
2022-06-15 19:06:39 -0500

    Update IERC721.sol

--- contracts/token/ERC721/IERC721.sol
+++ contracts/token/ERC721/IERC721.sol
@@ -3,7 +3,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../../utils/introspection/IERC165.sol";
+import "https://github.com/beatbreakerz/openzeppelin-contracts/blob/master/contracts/utils/introspection/IERC165.sol";
 
 /**
  * @dev Required interface of an ERC721 compliant contract.
